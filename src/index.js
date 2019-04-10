ea91a9218e53f806169250f4962bdbfb1d71153d
Taesu Hyeon <qvil1127@gmail.com>
2019-04-11 03:09:58 +0900

    Serving html file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // Server-side example
 const http2 = require("http2");
 const fs = require("fs");
+const path = require("path");
 
+const PORT = 8443;
 const server = http2.createSecureServer({
   key: fs.readFileSync("localhost-privkey.pem"),
   cert: fs.readFileSync("localhost-cert.pem")
@@ -14,7 +16,10 @@ server.on("stream", (stream, headers) => {
     "content-type": "text/html",
     ":status": 200
   });
-  stream.end("<h1>Hello World</h1>");
+  // stream.end("<h1>Hello World</h1>");
+  stream.end(fs.readFileSync(path.join(__dirname, "./index.html")));
 });
 
-server.listen(8443);
+server.listen(PORT, () => {
+  console.log(`Server learning https://localhost:${PORT} 🚀`);
+});
